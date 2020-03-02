f9c60b4b4494af0fab8f4853b050bbac78524b22
wasan <wasan.j@gmail.com>
2020-03-02 15:36:02 +0700

    Finish 2March

---
+++ lib/utility/my_style.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MyStyle {
+
+  // Field
+Color darkColor = Colors.purple.shade800;
+Color primaryColor = Colors.teal.shade700;
+
+  //Method
+MyStyle();
+
+}
--- lib/widget/authen.dart
+++ lib/widget/authen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:wasan/utility/my_style.dart';
 
 class Authen extends StatefulWidget {
   @override
@@ -11,6 +13,72 @@ class _AuthenState extends State<Authen> {
   // Field sakjdf;asldjf;alsjd sladfalskdjfl;ksadjfl
 
   // Method
+  Widget mySizebox() {
+    return SizedBox(
+      width: 5.0,
+    );
+  }
+
+  Widget signUpButton() {
+    return Expanded(
+      child: OutlineButton(
+        borderSide: BorderSide(color: MyStyle().primaryColor),
+        child: Text(
+          'SignUp',
+          style: TextStyle(color: Colors.white),
+        ),
+        onPressed: () {},
+      ),
+    );
+  }
+
+  Widget signInButton() {
+    return Expanded(
+      child: RaisedButton(
+        color: MyStyle().primaryColor,
+        child: Text(
+          'Sigh In',
+          style: TextStyle(color: Colors.white),
+        ),
+        onPressed: () {},
+      ),
+    );
+  }
+
+  Widget showButton() {
+    return Container(
+      color: Colors.lime,
+      width: 250.0,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisSize: MainAxisSize.max,
+        children: <Widget>[
+          signInButton(),
+          mySizebox(),
+          signUpButton(),
+        ],
+      ),
+    );
+  }
+
+  Widget passwordForm() {
+    return Container(
+      width: 250.0,
+      child: TextField(
+        obscureText: true,
+        decoration: InputDecoration(hintText: 'Password :'),
+      ),
+    );
+  }
+
+  Widget userForm() {
+    return Container(
+      width: 250.0,
+      child: TextField(
+        decoration: InputDecoration(hintText: 'User :'),
+      ),
+    );
+  }
 
   Widget showLogo() {
     return Container(
@@ -21,19 +89,38 @@ class _AuthenState extends State<Authen> {
   }
 
   Widget showAppName() {
-    return Text('RETURN GLO');
+    return Text(
+      'RETURN GLO',
+      style: GoogleFonts.tradeWinds(
+          textStyle: TextStyle(
+        color: MyStyle().darkColor,
+        fontStyle: FontStyle.italic,
+        fontWeight: FontWeight.bold,
+        fontSize: 30.0,
+      )),
+    );
   }
 
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-          child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          showLogo(),
-          showAppName(),
-        ],
-      )),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: RadialGradient(
+            colors: <Color>[Colors.white, MyStyle().primaryColor],radius: 1.0,
+          ),
+        ),
+        child: Center(
+            child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            showLogo(),
+            showAppName(),
+            userForm(),
+            passwordForm(),
+            showButton(),
+          ],
+        )),
+      ),
     );
   }
 }
